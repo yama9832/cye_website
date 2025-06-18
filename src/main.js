de92af40742765b244ed2d33ab08d5453f894779
hogehoge <yama9832@gmail.com>
2025-06-18 10:04:49 +0900

    googleアナリティクス実装

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vueGtag from 'vue-gtag-next'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+// Google Analyticsの設定を追加
+app.use(vueGtag, {
+  property: {
+    id: 'G-NS8FVF7K0V'
+  }
+})
+
+app.mount('#app')
